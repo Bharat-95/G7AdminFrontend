9488ffe8998c0e9e524da31c8d6793af969cb3af
Bharat020195 <tillu0201@gmail.com>
2024-05-29 18:13:53 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ''
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-rose-950">
+      <link rel="icon" href="../public/favicon.ico" />
       <body className={inter.className}>{children}</body>
     </html>
   );
