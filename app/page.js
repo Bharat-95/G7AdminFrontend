799a7c9feb25c62e38c4f535bb811952a4cf7a7c
G7cars <g7selfdrivecars@gmail.com>
2024-10-17 19:19:42 +0530

    a

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import Login from '../app/Login/page'
-const page = () => {
-  return (
-    <div>
-    <sign-in />
-    </div>
-  )
-}
+"use client";
+import { useSearchParams } from 'next/navigation';
+import { SignIn } from '@clerk/clerk-react';
 
-export default page
+const SignInPage = () => {
+  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -1,12 +1,5 @@
-import React from 'react'
 import { SignUp } from "@clerk/nextjs";
 
-const page = () => {
-  return (
-    <div>
-        <SignUp />
-    </div>
-  )
+export default function Page() {
+  return <div className="flex justify-center items-center h-screen"><SignUp /></div>;
 }
-
-export default page
