8484aadb05a60fd71a9a3ce1cbbf6a1c25315853
G7cars <g7selfdrivecars@gmail.com>
2024-10-17 19:26:24 +0530

    a

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Login from '../app/Login/page'
+import { SignIn } from '@clerk/nextjs'
 const page = () => {
   return (
     <div>
-    <Login />
+    <SignIn />
     </div>
   )
 }
