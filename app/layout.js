5b3dbe874594b2131b919d5a8942e093e3d83187
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 00:10:25 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -22,9 +22,7 @@ export default function RootLayout({ children }) {
         <SignedIn>
           {children}
           </SignedIn>
-          <SignedOut>
-            <SignIn/>
-          </SignedOut>
+        
       
         </body>
       </html>
