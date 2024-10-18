476e730f280000b5277597b6020ace52483e63c5
G7cars <g7selfdrivecars@gmail.com>
2024-10-18 17:15:15 +0530

    a

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import React from 'react'
+      import React from 'react'
+      import Signin from './sign-in/[[...sign-in]]/page'
 
-import { SignIn } from '@clerk/nextjs'
-const page = () => {
-  return (
-    <div className='flex justify-center items-center h-screen'>
-    <SignIn />
-    </div>
-  )
-}
 
-export default page
+      const page = () => {
+        return (
+          <div className='flex justify-center items-center h-screen'>
+            <Signin />
+          
+          </div>
+        )
+      }
+
+      export default page
