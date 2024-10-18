8eda069f3b9f0dd930dbb301f80bd895254f754d
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 02:32:38 +0530

    a

--- app/layout.js
+++ app/layout.js
@@ -19,12 +19,10 @@ export default function RootLayout({ children }) {
 <ClerkProvider >
       <html lang="en">
         <body>
-        <SignedIn>
+
           {children}
-          </SignedIn>
-        <SignedOut>
-          <Signin />
-        </SignedOut>
+    
+      
       
         </body>
       </html>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 import Header from './Header'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import Signin from '../app/sign-in/[[...sign-in]]/page'
+
+
 const page = () => {
   return (
     <div className=''>
+      <SignedIn>
      <Header />
+     </SignedIn>
+     <SignedOut>
+      <Signin/>
+     </SignedOut>
     </div>
   )
 }
