ab41524f719eb8d330d7db51d1d9bd957e6f11f8
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 01:15:45 +0530

    a

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -4,7 +4,7 @@ import React from 'react'
 const page = () => {
   return (
 
-        <SignIn path='https://accounts.g7admin.in/sign-in' />
+        <SignIn routing='https://accounts.g7admin.in/sign-in' path="/sign-in" />
 
   )
 }
