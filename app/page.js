b1294829320fdbd00b79393ae4454fdfc11cb15b
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 00:03:48 +0530

    a

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
       import React from 'react'
       import Signin from './sign-in/[[...sign-in]]/page'
+import { SignedIn, SignedOut } from '@clerk/nextjs'
+import Management from '../app/Management/page'
 
 
       const page = () => {
         return (
           <div className='flex justify-center items-center h-screen'>
-            <Signin />
+            <SignedIn>
+              <Management />
+            </SignedIn>
+            <SignedOut>
+            <Signin/>
+            </SignedOut>
           
           </div>
         )
