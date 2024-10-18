5c63e2fd8ebcdb04517a06417962cca7bc5a0fd6
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 00:16:39 +0530

    a

--- app/page.js
+++ app/page.js
@@ -3,8 +3,9 @@ import React from 'react'
 import Header from './Header'
 const page = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-     <Header />
+    <div>
+      <Header />
+     
     </div>
   )
 }
