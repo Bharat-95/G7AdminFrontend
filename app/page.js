f3459d73dbd571534b3e3128c0bad1279f3f2d8c
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 13:24:17 +0530

    a

--- app/page.js
+++ app/page.js
@@ -9,14 +9,15 @@ import Bg from '../public/1.png'
 
 const page = () => {
   return (
+    <div>
+       <SignedIn>
+     <Header />
+     </SignedIn>
     <div className='flex justify-center my-20 '>
  <div className='space-y-10 border p-20 rounded-xl text-white'>
       <Image
       src={Bg} 
       className='flex justify-center items-center h-40 w-80'/>
-      <SignedIn>
-     <Header />
-     </SignedIn>
      <SignedOut>
       <div className='flex justify-center items-center text-2xl text-white'>
         Welcome to G7 Admin
@@ -24,6 +25,7 @@ const page = () => {
       <div className='flex justify-center text-xl'><Link href="/sign-in" className='bg-black p-4 rounded-xl'>Login</Link></div>
      </SignedOut>
      </div>
+    </div>
     </div>
   )
 }
