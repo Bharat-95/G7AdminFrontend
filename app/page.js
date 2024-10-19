a0d8da7519d773d2a6768685bceee5769b0ea6e5
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 13:19:47 +0530

    a

--- app/page.js
+++ app/page.js
@@ -3,11 +3,17 @@ import React from 'react'
 import Header from './Header'
 import { SignedIn, SignedOut } from '@clerk/nextjs'
 import Link from 'next/link'
+import Image from 'next/image'
+import Bg from '../public/1.png'
 
 
 const page = () => {
   return (
-    <div className=''>
+    <div className='flex justify-center my-20 '>
+ <div className='space-y-10 border p-20 rounded-xl text-white'>
+      <Image
+      src={Bg} 
+      className='flex justify-center items-center h-40 w-80'/>
       <SignedIn>
      <Header />
      </SignedIn>
@@ -15,8 +21,9 @@ const page = () => {
       <div className='flex justify-center items-center text-2xl text-white'>
         Welcome to G7 Admin
       </div>
-      <div className='flex justify-center text-xl'>Please click here to <Link href="/sign-in">Login</Link></div>
+      <div className='flex justify-center text-xl'><Link href="/sign-in" className='bg-black p-4 rounded-xl'>Login</Link></div>
      </SignedOut>
+     </div>
     </div>
   )
 }
