0fa217297718edd3c601d2c62da293b59d2edb63
G7cars <g7selfdrivecars@gmail.com>
2024-10-19 13:54:59 +0530

    a

--- app/Documents/page.js
+++ app/Documents/page.js
@@ -59,13 +59,13 @@ const Page = () => {
   return (
     <SignedIn className="flex">
       <Header />
-      <div className="w-[100%] bg-white border-[2px] p-20 border-rose-900 rounded-xl">
+      <div className="w-[100%] bg-white border-[2px] p-4 border-rose-900 rounded-xl">
         <div className="text-rose-900 font-extrabold underline underline-offset-4 flex justify-center">
           VERIFY DOCUMENTS
         </div>
 
         {data.length === 0 ? (
-          <div className="text-center text-rose-900 font-semibold">
+          <div className="text-center m-20 text-rose-900 font-semibold">
             No Documents Found to Verify
           </div>
         ) : (
